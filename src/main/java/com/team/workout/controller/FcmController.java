11936b93aaefd83baabb8b4e016cbf0694786ba1
JIWON KIM <kkjim1324@naver.com>
2024-03-10 21:37:40 +0900

    update fcm

--- src/main/java/com/team/workout/controller/FcmController.java
+++ src/main/java/com/team/workout/controller/FcmController.java
@@ -1,10 +1,13 @@
 package com.team.workout.controller;
 
+import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.team.workout.dto.MessageRequest;
 import com.team.workout.service.FcmService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,13 +18,12 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-public class FcmController {
+public class FcmController implements GraphQLResolver {
 
     private final FcmService fcmService;
 
-    @PostMapping("/message/fcm/token")
-    public ResponseEntity sendMessageToken(@RequestBody MessageRequest requestDTO) throws IOException, FirebaseMessagingException {
+    @MutationMapping
+    public void sendMessageToken(@Argument(name = "message") MessageRequest requestDTO) throws FirebaseMessagingException {
         fcmService.sendMessageByToken(requestDTO.getTitle(), requestDTO.getBody(), requestDTO.getTargetToken());
-        return ResponseEntity.ok().build();
     }
 }
